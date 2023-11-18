015d518943f626aa3aace45e03ef9339b733a6ce
Thomas Bisgaard <thomas.bisgaard@sentia.dk>
2023-11-18 23:30:25 +0100

    implement Resource Folder

---
+++ internal/provider/folder_resource.go
@@ -0,0 +1,268 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	PPSClient "github.com/theochita/go-pleasant-password"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var _ resource.Resource = &FolderResource{}
+var _ resource.ResourceWithImportState = &FolderResource{}
+
+func NewfolderResource() resource.Resource {
+	return &FolderResource{}
+}
+
+// ExampleResource defines the resource implementation.
+type FolderResource struct {
+	client *PPSClient.APIClient
+	ctx    *context.Context
+}
+
+// ExampleResourceModel describes the resource data model.
+type FolderResourceModel struct {
+	Id       types.String `tfsdk:"id"`
+	Name     types.String `tfsdk:"name"`
+	ParentID types.String `tfsdk:"parentid"`
+	Notes    types.String `tfsdk:"notes"`
+}
+
+func (r *FolderResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_folder"
+}
+
+func (r *FolderResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		// This description is used by the documentation generator and the language server.
+		MarkdownDescription: "Example resource",
+
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				MarkdownDescription: "Example configurable attribute",
+				Computed:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"name": schema.StringAttribute{
+				MarkdownDescription: "Example identifier",
+				Required:            true,
+			},
+			"parentid": schema.StringAttribute{
+				MarkdownDescription: "Example identifier",
+				Optional:            true,
+				Computed:            true,
+			},
+			"notes": schema.StringAttribute{
+				MarkdownDescription: "Example identifier",
+				Optional:            true,
+				Computed:            true,
+			},
+		},
+	}
+}
+
+func (r *FolderResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	providerclient, ok := req.ProviderData.(ProviderClient)
+
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *PPSClient.APIClient, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	r.client = &providerclient.Client
+	r.ctx = &providerclient.Ctx
+
+}
+
+func (r *FolderResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data FolderResourceModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// If applicable, this is a great opportunity to initialize any necessary
+	// provider client data and make a call using it.
+	// httpResp, err := r.client.Do(httpReq)
+	// if err != nil {
+	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to create example, got error: %s", err))
+	//     return
+	// }
+
+	// For the purposes of this example code, hardcoding a response value to
+	// save into the Terraform state.
+
+	param := PPSClient.NewV6CredentialGroupInputWithDefaults()
+	param.Name = data.Name.ValueStringPointer()
+	param.Notes = data.Notes.ValueStringPointer()
+	param.ParentId = data.ParentID.ValueStringPointer()
+
+	res, httpres, err := r.client.DefaultAPI.PostV6Folders(*r.ctx).V6CredentialGroupInput(*param).Execute()
+
+	if err != nil {
+		resp.Diagnostics.AddError("failure to invoke API: ", err.Error())
+		return
+	}
+	if httpres.StatusCode != 200 {
+		resp.Diagnostics.AddError("Got an unexpected response code", fmt.Sprintf("Got an unexpected response code %v", httpres.StatusCode))
+		return
+	}
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	sanityresult := strings.Trim(res, "\"\\") //remove quotes and backslashes from result
+	data.Id = types.StringValue(sanityresult)
+	data.Name = types.StringValue(param.GetName())
+	data.Notes = types.StringValue(param.GetNotes())
+	data.ParentID = types.StringValue(param.GetParentId())
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *FolderResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var data FolderResourceModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	res, httpres, err := r.client.DefaultAPI.GetV6FoldersByID(*r.ctx, data.Id.ValueString()).Execute()
+
+	if err != nil {
+		resp.Diagnostics.AddError("failure to invoke API: ", err.Error())
+		return
+	}
+	if httpres.StatusCode != 200 {
+		resp.Diagnostics.AddError("Got an unexpected response code", fmt.Sprintf("Got an unexpected response code %v", httpres.StatusCode))
+		return
+	}
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	data.Id = types.StringValue(res.GetId())
+	data.Name = types.StringValue(res.GetName())
+	data.Notes = types.StringValue(res.GetNotes())
+	data.ParentID = types.StringValue(res.GetParentId())
+
+	// If applicable, this is a great opportunity to initialize any necessary
+	// provider client data and make a call using it.
+	// httpResp, err := r.client.Do(httpReq)
+	// if err != nil {
+	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to read example, got error: %s", err))
+	//     return
+	// }
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *FolderResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var data FolderResourceModel
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	param := PPSClient.NewV6CredentialGroupInputWithDefaults()
+	param.Name = data.Name.ValueStringPointer()
+	param.Notes = data.Notes.ValueStringPointer()
+	param.ParentId = data.ParentID.ValueStringPointer()
+
+	httpres, err := r.client.DefaultAPI.PatchV6FoldersByID(*r.ctx, data.Id.ValueString()).V6CredentialGroupInput(*param).Execute()
+
+	if err != nil {
+		resp.Diagnostics.AddError("failure to invoke API: ", err.Error())
+		return
+	}
+	if httpres.StatusCode != 204 {
+		resp.Diagnostics.AddError("Got an unexpected response code", fmt.Sprintf("Got an unexpected response code %v", httpres.StatusCode))
+		return
+	}
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	// If applicable, this is a great opportunity to initialize any necessary
+	// provider client data and make a call using it.
+	// httpResp, err := r.client.Do(httpReq)
+	// if err != nil {
+	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to update example, got error: %s", err))
+	//     return
+	// }
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *FolderResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var data FolderResourceModel
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	httpres, err := r.client.DefaultAPI.DeleteV6FoldersByID(*r.ctx, data.Id.ValueString()).Execute()
+
+	if err != nil {
+		resp.Diagnostics.AddError("failure to invoke API: ", err.Error())
+		return
+	}
+	if httpres.StatusCode != 204 {
+		resp.Diagnostics.AddError("Got an unexpected response code", fmt.Sprintf("Got an unexpected response code %v", httpres.StatusCode))
+		return
+	}
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// If applicable, this is a great opportunity to initialize any necessary
+	// provider client data and make a call using it.
+	// httpResp, err := r.client.Do(httpReq)
+	// if err != nil {
+	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to delete example, got error: %s", err))
+	//     return
+	// }
+}
+
+func (r *FolderResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
